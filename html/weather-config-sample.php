7b3f77a14475bfd27c06ad41c0d3bd3d8d049378
root <root@ip-172-26-3-122.ec2.internal>
2019-07-08 10:58:43 -0400

    Added config sample file

---
+++ html/weather-config-sample.php
@@ -0,0 +1,4 @@
+<?php 
+$dark_sky_api_key = '68aef0fda29b6c9c8ecc9ae8ee48346c'; // Your Dark Sky API key.
+$forecast_latitude = 40.730610; // Decimal notation of forecast point latitude.
+$forecast_longitude = -73.935242; // Decimal notation of forecast point longitude.
